f54e915f74a5c17e02ff9031019898955d8defe1
mikitosina1 <mikitosina@mail.ru>
2021-03-31 23:34:44 +0300

    styles
    css direction

--- public/miki_components/nav_panel.php
+++ public/miki_components/nav_panel.php
@@ -1,14 +1,15 @@
 <nav class="navbar navbar-expand-lg flex-column flex-md-row" role="navigation">
-    <div class="logo__part">
-        <a class="navbar-brand" href="/">
-            <img src="../miki_styles/images/logo-03.svg" alt="Логотип сайта" title="Логотип сайта" class="logo_header">
-            <div class="title__blog">Mikitosina's Blog :D</div>
-        </a>
-    </div>
+	<div class="logo__part">
+		<a class="navbar-brand" href="/">
+			<img src="../miki_styles/images/blog.svg" alt="Логотип сайта" title="Логотип сайта" class="logo_header">
+			<div class="title__blog">Mikitosina's Blog :D</div>
+			<div class="hidden">Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+		</a>
+	</div>
 	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 		<span class="navbar-toggler-icon">
-            <img src="../miki_styles/images/hamburger.svg" alt="hamburger">
-        </span>
+			<img src="../miki_styles/images/hamburger.svg" alt="hamburger">
+		</span>
 	</button>
 	<div class="collapse navbar-collapse" id="navbarSupportedContent">
 		<ul class="navbar-nav mr-auto">
@@ -27,11 +28,11 @@
 				Hometask
 				</a>
 				<div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="../1homework/twitter/index.html">Twitter</a>
-                    <a class="dropdown-item" href="../1homework/2hw/index.php">parser</a>
-                    <a class="dropdown-item" href="../1homework/3hw/index.php">3hw</a>
-                    <a class="dropdown-item" href="../work_project/index.html">Тиснение</a>
-                    <a class="dropdown-item" href="../1homework/Laravel-homework/laravel_app/public/index.php">Laravel-test_app</a>
+					<a class="dropdown-item" href="../1homework/twitter/index.html">Twitter</a>
+					<a class="dropdown-item" href="../1homework/2hw/index.php">parser</a>
+					<a class="dropdown-item" href="../1homework/3hw/index.php">3hw</a>
+					<a class="dropdown-item" href="../work_project/index.html">Тиснение</a>
+					<a class="dropdown-item" href="../1homework/Laravel-homework/laravel_app/public/index.php">Laravel-test_app</a>
 				</div>
 			</li>
 			<?php if (is_guest()):?>
@@ -53,9 +54,9 @@
 				</li>
 			<?php endif;?>
 		</ul>
-		<form class="form-inline my-2 my-lg-0">
+<!-- 		<form class="form-inline my-2 my-lg-0">
 			<input class="form-control mr-sm-2" type="search" placeholder="в разработке :)" aria-label="Search">
 			<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-		</form>
+		</form> -->
 	</div>
 </nav>
