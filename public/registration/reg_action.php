012fe894a2b80b7e67679292230c66c75f3c937e
mikitosina1 <mikitosina@mail.ru>
2021-05-20 00:16:47 +0300

    registration update
    rewrite

--- public/registration/reg_action.php
+++ public/registration/reg_action.php
@@ -6,16 +6,21 @@ session_start();
 
 require_once "../components/main_func.php";
 
-$u_email = trim(filter_var($_POST['u_email'], FILTER_SANITIZE_EMAIL));
-$u_login = trim(filter_var($_POST['u_login'], FILTER_SANITIZE_STRING));
-$u_password = trim(filter_var($_POST['u_password'], FILTER_SANITIZE_STRING));
-$u_password = hashPassword($u_password);
-$username = trim(filter_var($_POST['Fname'], FILTER_SANITIZE_STRING));
-$userlastname = trim(filter_var($_POST['Lname'], FILTER_SANITIZE_STRING));
+$trim_u_email = trim(filter_var($_POST['u_email'], FILTER_SANITIZE_EMAIL));
+$trim_u_login = trim(filter_var($_POST['u_login'], FILTER_SANITIZE_STRING));
+$trim_u_password = trim(filter_var($_POST['u_password'], FILTER_SANITIZE_STRING));
+$trim_username = trim(filter_var($_POST['Fname'], FILTER_SANITIZE_STRING));
+$trim_userlastname = trim(filter_var($_POST['Lname'], FILTER_SANITIZE_STRING));
+$u_email = htmlspecialchars($trim_u_email);
+$u_login = htmlspecialchars($trim_u_email);
+$pre_u_password = htmlspecialchars($trim_u_password);
+$u_password = hashPassword($pre_u_password);
+$username = htmlspecialchars($trim_username);
+$userlastname = htmlspecialchars($trim_userlastname);
 $u_age = $_POST['u_age'];
 $u_info = $_POST['u_info'];
 $user_role = 'user';
-$u_ip = isset($_SERVER['HTTP_CLIENT_IP']) ? $_SERVER['HTTP_CLIENT_IP'] : isset($_SERVER['HTTP_X_FORWARDED_FOR']) ? $_SERVER['HTTP_X_FORWARDED_FOR'] : $_SERVER['REMOTE_ADDR'];
+$u_ip = isset($_SERVER['HTTP_CLIENT_IP']) ? $_SERVER['HTTP_CLIENT_IP'] : (isset($_SERVER['HTTP_X_FORWARDED_FOR']) ? $_SERVER['HTTP_X_FORWARDED_FOR'] : $_SERVER['REMOTE_ADDR']);
 
 if (strlen($u_email) <= 3)
 	die('Упс, имейл не соответствует требованиям заполнения');
