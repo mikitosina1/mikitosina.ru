4ffd267318164eaf81ff27da464e11d1f716e92c
mikitosina1 <mikitosina@mail.ru>
2021-06-26 15:37:56 +0300

    reg style remaking
    all on flex

--- public/registration/reg.php
+++ public/registration/reg.php
@@ -26,41 +26,49 @@ if($user_handler->is_guest() == TRUE){
 ?>
 <div class="container reg_form_container">
 	<div class="row">
-		<div class="col-sm"></div>
 		<div class="col-sm reg_form_content mt-4">
 			<form action="reg_action.php" method="post">
-				<label><p><b style="color: #880000; font-size: 1.5em;">*</b> - очень важные поля, без которых невозможно зарегистрироваться.</p></label>
-				<label for="u_email">Введите Вашу электронную почту</label>
-				<input type="email" name="u_email" id="u_email" class="form-control">
+				<label class="markplace"><p><b style="color: #880000; font-size: 1.5em;">*</b> - очень важные поля, без которых невозможно зарегистрироваться.</p></label>
+				<div class="formpart">
+					<label for="u_email">Введите Вашу электронную почту</label>
+					<input type="email" name="u_email" id="u_email" class="form-control">
+				</div>
+				<div class="formpart">
+					<label for="u_login">Ваш<strong style="color: #2d6ca2"> Логин </strong><b style="color: #880000; font-size: 1.5em;">*</b>(не менее 3-х символов)</label>
+					<input type="text" name="u_login" id="u_login" class="form-control">
+				</div>
+				<div class="formpart">
+					<label for="u_password" class="passLabel">
+						Придумайте<strong style="color: #3e8f3e"> Пароль</strong><b style="color: #880000; font-size: 1.5em;">*</b>
+						<br>Не менее 8 символов, заглавные и строчные буквы, цифры (обязательное условие)
+					</label>
+					<input type="password" name="u_password" id="u_password" class="form-control">
+				</div>
 
-				<label for="u_login">Ваш<strong style="color: #2d6ca2"> логин </strong><b style="color: #880000; font-size: 1.5em;">*</b> <br> (не менее 3-х символов)</label>
-				<input type="text" name="u_login" id="u_login" class="form-control">
-
-				<label for="u_password"> <strong style="color: #3e8f3e">Пароль</strong><b style="color: #880000; font-size: 1.5em;">*</b> </br>
-					Не менее 8 символов, заглавные и строчные буквы, цифры </br>
-								(обязательное условие)
-				</label>
-				<input type="password" name="u_password" id="u_password" class="form-control">
-
-				<label for="u_password2">Повторите <strong style="color: #3e8f3e">пароль</strong><b style="color: #880000; font-size: 1.5em;">*</b></label>
-				<input type="password" name="u_password2" id="u_password2" class="form-control">
-
-				<label for="Fname">Имя</label>
-				<input type="text" name="Fname" id="Fname" class="form-control">
-
-				<label for="Lname">Фамилия</label>
-				<input type="text" name="Lname" id="Lname" class="form-control">
-
-				<label for="u_age">Возраст</label>
-				<input type="text" name="u_age" id="u_age" class="form-control">
-
-				<label for="u_info">Немного автобиографии</label>
-				<input type="text" name="u_info" id="u_info" class="form-control">
+				<div class="formpart">
+					<label for="u_password2">Повторите <strong style="color: #3e8f3e">пароль</strong><b style="color: #880000; font-size: 1.5em;">*</b></label>
+					<input type="password" name="u_password2" id="u_password2" class="form-control">
+				</div>
+				<div class="formpart">
+					<label for="Fname">Имя</label>
+					<input type="text" name="Fname" id="Fname" class="form-control">
+				</div>
+				<div class="formpart">
+					<label for="Lname">Фамилия</label>
+					<input type="text" name="Lname" id="Lname" class="form-control">
+				</div>
+				<div class="formpart">
+					<label for="u_age">Возраст</label>
+					<input type="text" name="u_age" id="u_age" class="form-control">
+				</div>
+				<div class="formpart">
+					<label for="u_info">Немного автобиографии</label>
+					<input type="text" name="u_info" id="u_info" class="form-control">
+				</div>
 
 				<button type="submit" class="btn btn_reg_regaction btn-success mt-4">Зарегистрироваться</button>
 			</form>
 		</div>
-		<div class="col-sm"></div>
 	</div>
 </div>
 <footer>
